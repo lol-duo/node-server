234934ec32511ce48c399e7eb4acdcaae72e18bc
imygnam <imygnam@gmail.com>
2023-07-02 18:50:42 +0900

    [FIX] add log

--- lolDuo-api/src/index.js
+++ lolDuo-api/src/index.js
@@ -2,15 +2,16 @@ import LeagueController from "./controller/LeagueController.js";
 import SlackService from "./service/slack.js";
 import dotenv from "dotenv";
 import express from "express";
-
+console.log("0");
 if(process.env.MODE === "dev"){
     dotenv.config({path: process.env.npm_config_local_prefix + "/secret/secret.env"});
 }
 if(process.env.MODE === "prod"){
+    console.log("lol-duo api server is running");
     const slackService = SlackService.getInstance();
     slackService.sendMessage(process.env.Slack_Channel, "lol-duo api server is running");
 }
-
+console.log("1");
 const controller = new LeagueController();
 
 let app = new express();
